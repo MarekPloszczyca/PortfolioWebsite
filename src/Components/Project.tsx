ae7176deee8e4d75837815e90dc239104104f4b1
Marobuc <marploszczyca@gmail.com>
2023-11-18 11:59:08 +0100

    bug with resizing fixed, started adding my projects to projects section

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -1,20 +1,52 @@
 import styles from "./Project.module.scss";
-import Dummy from "../Assets/dummy.jpg";
 import LinkLightIcon from "../Assets/link-light.png";
 import LinkDarkIcon from "../Assets/link-dark.png";
 import GitHubLight from "../Assets/github-light.png";
 import GitHubDark from "../Assets/github-dark.png";
 import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
 
 interface Props {
   style: string | undefined;
+  screenshots: string[];
 }
 
 export default function Project(props: Props) {
   const mode = useSelector((state: { dark: boolean }) => state.dark);
+  const [newInterval, setNewInterval] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const interval = useRef<number>();
+
+
+  
+  useEffect(() => {
+    if (!newInterval) {
+      return clearInterval(interval.current);
+    }
+    if (newInterval) {
+      interval.current = setInterval(() => {
+        setSlideNumber((current) => {
+          if (current === 2) {
+            return current - 2;
+          }
+          return current + 1;
+        });
+      }, 2000);
+    }
+  }, [newInterval]);
+
   return (
-    <div className={`${props.style} + ${styles.project}`}>
-      <img src={Dummy}></img>
+    <div
+      onMouseEnter={() => {
+        setNewInterval(true);
+      }}
+      onMouseLeave={() => {
+        setNewInterval(false);
+        setSlideNumber(0);
+      }}
+      className={`${props.style} + ${styles.project}`}
+    >
+      <img src={props.screenshots[slideNumber]}></img>
       <h5>Project title goes here</h5>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
--- src/Components/ProjectGrid.tsx
+++ src/Components/ProjectGrid.tsx
@@ -1,6 +1,14 @@
 import Project from "./Project";
 import styles from "./ProjectGrid.module.scss";
-
+import LinkLightIcon from "../Assets/link-light.png";
+import LinkDarkIcon from "../Assets/link-dark.png";
+import GitHubLight from "../Assets/github-light.png";
+import PassGenOne from "../Assets/WebsitesScreenshots/PassGen1.jpg";
+import PassGenTwo from "../Assets/WebsitesScreenshots/PassGen2.jpg";
+import PassGenThree from "../Assets/WebsitesScreenshots/PassGen3.jpg";
+import ConnectOne from "../Assets/WebsitesScreenshots/ConnectFour1.jpg";
+import ConnectTwo from "../Assets/WebsitesScreenshots/ConnectFour2.jpg";
+import ConnectThree from "../Assets/WebsitesScreenshots/ConnectFour3.jpg";
 interface Props {
   projectsFirstDisplay: boolean;
   projectsSecondDisplay: boolean;
@@ -10,19 +18,23 @@ export default function ProjectGrid(props: Props) {
   return (
     <div className={styles.projectGrid}>
       <Project
+        screenshots={[LinkLightIcon, LinkDarkIcon, GitHubLight]}
         style={
           props.projectsFirstDisplay ? styles.firstDisplayed : styles.leftOne
         }
       />
       <Project
+        screenshots={[GitHubLight, LinkLightIcon, LinkDarkIcon]}
         style={
           props.projectsFirstDisplay ? styles.secondDisplayed : styles.rightOne
         }
       />
       <Project
+        screenshots={[ConnectOne, ConnectTwo, ConnectThree]}
         style={props.projectsSecondDisplay ? styles.leftTwo : undefined}
       />
       <Project
+        screenshots={[PassGenOne, PassGenTwo, PassGenThree]}
         style={props.projectsSecondDisplay ? styles.rightTwo : undefined}
       />
     </div>
