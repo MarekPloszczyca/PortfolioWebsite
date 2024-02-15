1ad356b545f6126fef3bcef5954168a806912dae
Marobuc <marploszczyca@gmail.com>
2024-02-15 17:10:58 +0100

    changed styling of single project

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -9,10 +9,10 @@ import { useEffect, useRef, useState } from "react";
 interface Props {
   style: string | undefined;
   screenshots: string[];
-  title:string;
-  description:string;
-  liveHref:string;
-  codeHref:string;
+  title: string;
+  description: string;
+  liveHref: string;
+  codeHref: string;
 }
 
 export default function Project(props: Props) {
@@ -21,8 +21,6 @@ export default function Project(props: Props) {
   const [slideNumber, setSlideNumber] = useState(0);
   const interval = useRef<number>();
 
-
-  
   useEffect(() => {
     if (!newInterval) {
       return clearInterval(interval.current);
@@ -48,21 +46,29 @@ export default function Project(props: Props) {
         setNewInterval(false);
         setSlideNumber(0);
       }}
-      className={`${props.style} + ${styles.project}`}
+      className={`${props.style} + ${
+        mode ? styles.darkProject : styles.project
+      }`}
     >
       <img src={props.screenshots[slideNumber]}></img>
-      <div className={styles.descriptionContainer}><h5>{props.title}</h5>
-      <p>
-     {props.description}
-      </p>
-      <div>
-      <a href={props.liveHref} target="_blank"> <p>
-          <img src={mode ? LinkDarkIcon : LinkLightIcon}></img>Live preview
-        </p></a>
-        <a href={props.codeHref} target="_blank"> <p>
-         <img src={mode ? GitHubDark : GitHubLight}></img>View code
-        </p></a>
-      </div></div>
+      <div className={styles.descriptionContainer}>
+        <h5>{props.title}</h5>
+        <p>{props.description}</p>
+        <div>
+          <a href={props.liveHref} target="_blank">
+            {" "}
+            <p>
+              <img src={mode ? LinkDarkIcon : LinkLightIcon}></img>Live preview
+            </p>
+          </a>
+          <a href={props.codeHref} target="_blank">
+            {" "}
+            <p>
+              <img src={mode ? GitHubDark : GitHubLight}></img>View code
+            </p>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
